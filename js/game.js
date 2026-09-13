218892076a4d4e0ffa7b7505a2a3f1be0da92f59
brink_laptop <285325146+BrinkRiver@users.noreply.github.com>
2026-09-13 22:25:11 +0900

    fix: Add constant definitions and format code.

--- js/game.js
+++ js/game.js
@@ -1,17 +1,32 @@
+import { PLAYER } from "./board.js";
+
+export const GAME_STATUS = {
+    NORMAL: "normal",
+    PASS: "pass",
+    GAME_OVER: "gameover",
+    INVALID: "invalid",
+};
+
 export class Game {
     constructor(board) {
         this.board = board;
-        this.currentPlayer = "black";
+        this.currentPlayer = PLAYER.BLACK;
         this.gameOver = false;
     }
 
     play(row, column) {
         if (this.gameOver) {
-            return { success: false};
+            return {
+                status: GAME_STATUS.INVALID,
+                player: this.currentPlayer,
+            };
         }
 
         if (!this.board.isValidMove(row, column, this.currentPlayer)) {
-            return { success: false};
+            return {
+                status: GAME_STATUS.INVALID,
+                player: this.currentPlayer,
+            };
         }
 
         this.board.placeDisk(row, column, this.currentPlayer);
@@ -21,47 +36,42 @@ export class Game {
         if (this.board.remainValidMove(nextPlayer)) {
             this.currentPlayer = nextPlayer;
             return {
-                success: true,
-                status: "normal",
-            }
+                status: GAME_STATUS.NORMAL,
+                player: this.currentPlayer,
+            };
         }
 
         if (this.board.remainValidMove(this.currentPlayer)) {
             return {
-                success: true,
-                status: pass,
-                player: nextPlayer
-            }
+                status: GAME_STATUS.PASS,
+                player: nextPlayer,
+            };
         }
 
         this.gameOver = true;
 
         return {
-            success: true,
-            status: "gameover",
-        }
+            status: GAME_STATUS.GAME_OVER,
+            player: nextPlayer,
+        };
     }
 
     getNextPlayer() {
-        if (this.currentPlayer === "black") {
-            return "white";
+        if (this.currentPlayer === PLAYER.BLACK) {
+            return PLAYER.WHITE;
         } else {
-            return "black";
+            return PLAYER.BLACK;
         }
     }
 
     reset() {
         this.board.reset();
-        this.currentPlayer = "black";
+        this.currentPlayer = PLAYER.BLACK;
         this.gameOver = false;
     }
 
     isValidMove(row, column) {
-        return this.board.isValidMove(
-            row,
-            column,
-            this.currentPlayer
-        )
+        return this.board.isValidMove(row, column, this.currentPlayer);
     }
 
     canPlace(row, column) {
--- js/main.js
+++ js/main.js
@@ -1,69 +1,78 @@
-import { Board, BOARD_SIZE } from "./board.js";
-import { Game } from "./game.js"
+import { Board, BOARD_SIZE, PLAYER } from "./board.js";
+import { Game, GAME_STATUS } from "./game.js";
 
 const boardElement = document.querySelector("#board");
 const turnElement = document.querySelector("#turn");
 const blackCountElement = document.querySelector("#black-count");
-const whiteCountElement = document.querySelector("#white-count")
+const whiteCountElement = document.querySelector("#white-count");
 const gameMessageElement = document.querySelector("#game-message");
 const resetButton = document.querySelector("#reset-button");
 
 const board = new Board();
 const game = new Game(board);
 
-for (let row = 0; row < BOARD_SIZE; row++) {
-    for (let column = 0; column < BOARD_SIZE; column++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-
-        cell.dataset.row = row;
-        cell.dataset.column = column;
-
-        cell.addEventListener("click", (event) => {
-            const row = Number(event.currentTarget.dataset.row);
-            const column = Number(event.currentTarget.dataset.column);
-
-            const result = game.play(row, column);
-
-            if (!result.success) {
-                return;
-            }
-
-            cell.classList.remove("preview-black", "preview-white");
-
-            render();
-            renderGameStatus(result);
-        });
-
-        cell.addEventListener("mouseenter", () => {
-            const row = Number(cell.dataset.row);
-            const column = Number(cell.dataset.column);
-
-            if (game.canPlace(row, column)) {
-                if (game.isValidMove(row, column)) {
-                    cell.classList.add(`preview-${game.currentPlayer}`);
-                }
-            }
-        })
-        cell.addEventListener("mouseleave", () => {
-            cell.classList.remove("preview-black", "preview-white");
-        })
-        boardElement.appendChild(cell);
-    }
-}
-
-resetButton.addEventListener("click", () => {
-    game.reset();
-    gameMessageElement.textContent = "";
-    gameMessageElement.style.display = "none";
+function init() {
+    createBoard();
+    createResetButton();
     render();
-})
-
-render();
+}
+init();
 
 // ---------------------
 // Functions
 // ---------------------
+function createBoard() {
+    for (let row = 0; row < BOARD_SIZE; row++) {
+        for (let column = 0; column < BOARD_SIZE; column++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+
+            cell.dataset.row = row;
+            cell.dataset.column = column;
+
+            cell.addEventListener("click", (event) => {
+                const row = Number(event.currentTarget.dataset.row);
+                const column = Number(event.currentTarget.dataset.column);
+
+                const result = game.play(row, column);
+
+                if (result.status == GAME_STATUS.INVALID) {
+                    return;
+                }
+
+                cell.classList.remove("preview-black", "preview-white");
+
+                render();
+                renderGameStatus(result.status);
+            });
+
+            cell.addEventListener("mouseenter", () => {
+                const row = Number(cell.dataset.row);
+                const column = Number(cell.dataset.column);
+
+                if (game.canPlace(row, column)) {
+                    if (game.isValidMove(row, column)) {
+                        cell.classList.add(`preview-${game.currentPlayer}`);
+                    }
+                }
+            });
+            cell.addEventListener("mouseleave", () => {
+                cell.classList.remove("preview-black", "preview-white");
+            });
+            boardElement.appendChild(cell);
+        }
+    }
+}
+
+function createResetButton() {
+    resetButton.addEventListener("click", () => {
+        game.reset();
+        gameMessageElement.textContent = "";
+        gameMessageElement.style.display = "none";
+        render();
+    });
+}
+
 function renderBoard() {
     const cells = boardElement.querySelectorAll(".cell");
 
@@ -72,24 +81,24 @@ function renderBoard() {
             const index = row * board.cells[row].length + column;
             const cell = cells[index];
 
-            cell.classList.remove("black", "white");
+            cell.classList.remove(PLAYER.BLACK, PLAYER.WHITE);
 
             const disk = board.cells[row][column];
 
-            if (disk === "black") {
-                cell.classList.add("black");
+            if (disk === PLAYER.BLACK) {
+                cell.classList.add(PLAYER.BLACK);
             }
-            if (disk === "white") {
-                cell.classList.add("white");
+            if (disk === PLAYER.WHITE) {
+                cell.classList.add(PLAYER.WHITE);
             }
         }
     }
 }
 
 function renderTurn() {
-    const player = 
-        game.currentPlayer.charAt(0).toUpperCase()
-        + game.currentPlayer.slice(1);
+    const player =
+        game.currentPlayer.charAt(0).toUpperCase() +
+        game.currentPlayer.slice(1);
     turnElement.textContent = `${player}`;
 }
 
@@ -100,13 +109,12 @@ function renderDiskCount() {
     whiteCountElement.textContent = counts.white;
 }
 
-function renderGameStatus(result) {
-    if (result.status === "pass") {
-        gameMessageElement.textContent =
-            `${result.player} has no valid moves.`;
+function renderGameStatus(status) {
+    if (status === GAME_STATUS.PASS) {
+        gameMessageElement.textContent = `${result.player} has no valid moves.`;
     }
 
-    if (result.status === "gameover") {
+    if (status === GAME_STATUS.GAME_OVER) {
         const counts = board.countDisks();
 
         if (counts.black > counts.white) {
@@ -120,6 +128,7 @@ function renderGameStatus(result) {
         gameMessageElement.style.display = "block";
     }
 }
+
 function render() {
     renderBoard();
     renderTurn();
